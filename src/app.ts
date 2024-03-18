0aba001905fc7a2917f257d37bac1bf3ba75e545
Lucas Gabriel <lucasgabriel.shig@hotmail.com>
2024-03-17 22:49:36 -0300

    feat: integration

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use("/", router);
 
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: "https://desafio-fullstack-shigekai-frontend.vercel.app/"
 }));
 
 app.use(handleError);
